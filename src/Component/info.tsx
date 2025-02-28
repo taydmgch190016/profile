7c3b31b58bc148a44d1dc999e7a47f7e9a568a9c
taydmgch190016 <vaperminhtay150601@gmail.com>
2025-02-28 19:05:11 +0700

    Wrap

--- src/Component/info.tsx
+++ src/Component/info.tsx
@@ -19,6 +19,7 @@ export default function Info() {
           flexDirection: "row",
           width: "100vw",
           justifyContent: "space-around",
+          flexWrap: "wrap"
         }}
       >
         <div style={{ alignItems: "center" }}>
